5609c4baf38b61e642b5dbd530bc7c0f7cfd5149
357932982 <357932982@qq.com>
2018-02-26 18:33:54 +0800

    undertake login function

--- dailyfresh/df_user/urls.py
+++ dailyfresh/df_user/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^register$', views.register, name='register'),
-    url(r'^register_handler$', views.register_handler, name='register_handler'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^login_handler/$', views.login_handler, name='login_handler'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^register_handler/$', views.register_handler, name='register_handler'),
     url(r'^user_name_validate/$', views.user_name_validate, name='user_name_validate'),
+    url(r'^info/$', views.info, name='info'),
+    url(r'^order/$', views.order, name='order'),
+    url(r'^site/$', views.site, name='site'),
 ]
--- dailyfresh/df_user/views.py
+++ dailyfresh/df_user/views.py
@@ -15,12 +15,11 @@ def register_handler(request):
     user_name = request.POST['user_name']
     pwd = request.POST['pwd']
     email = request.POST['email']
-
     # 密码加密
     s1 = sha1()
     s1.update(pwd.encode('utf-8'))
     pwd = s1.hexdigest()
-    # user = UserInfo.user.create_user(user_name, pwd, email)
+    user = UserInfo.user.create_user(user_name, pwd, email)
 
     return HttpResponse(pwd)
 
@@ -30,3 +29,33 @@ def user_name_validate(request):
     count = UserInfo.user.filter(user_name=name).count()
     print(count)
     return HttpResponse(count)
+
+
+def login(request):
+    return render(request, 'df_user/login.html', {'title': '登录'})
+
+
+def login_handler(request):
+    post = request.POST
+    user_name = post.get('username')
+    pwd = post.get('pwd')
+    remember = post.get('remember', None)
+    name = UserInfo.user.filter(user_name=user_name)
+    if 0 == len(name):
+        error_user = True
+        context = {'title': '登录', 'error_user': error_user, 'user_name': user_name, 'pwd': pwd}
+        return render(request, 'df_user/login.html', context)
+        
+    return HttpResponse('%s, %s, %s' % (user_name, pwd, remember))
+
+
+def info(request):
+    return render(request, 'df_user/user_center_info.html', {'title': '用户中心'})
+
+
+def order(request):
+    return render(request, 'df_user/user_center_order.html', {'title': '用户中心'})
+
+
+def site(request):
+    return render(request, 'df_user/user_center_site.html', {'title': '用户中心'})
