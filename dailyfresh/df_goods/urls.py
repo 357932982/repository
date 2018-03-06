9f2efba60ef20feb8f6c4b6ce0355a0e64195108
357932982 <357932982@qq.com>
2018-03-07 00:03:09 +0800

    finish df_goods page'adjast

--- dailyfresh/df_goods/urls.py
+++ dailyfresh/df_goods/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^index', views.index, name='index'),
-    url(r'^list', views.get_list, name='get_list'),
-    url(r'^cart', views.cart, name='cart'),
-    url(r'^detail', views.detail, name='detail'),
-    url(r'^place_order', views.place_order, name='place_order'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^list/$', views.get_list, name='get_list'),
+    url(r'^cart/$', views.cart, name='cart'),
+    url(r'^detail/$', views.detail, name='detail'),
+    url(r'^place_order/$', views.place_order, name='place_order'),
 ]
--- dailyfresh/df_goods/views.py
+++ dailyfresh/df_goods/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'df_goods/index.html')
+    return render(request, 'df_goods/index.html', {'title': '首页'})
 
 
 def get_list(request):
-    return render(request, 'df_goods/list.html')
+    return render(request, 'df_goods/list.html', {'title': '商品列表'})
 
 
 def cart(request):
-    return render(request, 'df_goods/cart.html')
+    return render(request, 'df_goods/cart.html', {'title': '购物车'})
 
 
 def detail(request):
-    return render(request, 'df_goods/details.html')
+    return render(request, 'df_goods/detail.html', {'title': '商品详情'})
 
 
 def place_order(request):
-    return render(request, 'df_goods/place_order.html')
+    return render(request, 'df_goods/place_order.html', {'title': '提交订单'})
